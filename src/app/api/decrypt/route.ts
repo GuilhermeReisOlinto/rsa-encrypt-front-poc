b14cc30d18a3d581ba3c7cf85b4edab9073c64da
guilherme olinto <ctl_golinto@uolinc.com>
2025-04-29 16:42:21 -0300

    feat: alter repo of encryption

--- src/app/api/decrypt/route.ts
+++ src/app/api/decrypt/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { decryptPayload } from '../../hook/decryptBack';
+import { decryptPayload } from '../../../shared/encryption/decryptBack';
 
 export async function POST(req: NextRequest) {
     const dados = await req.json();
